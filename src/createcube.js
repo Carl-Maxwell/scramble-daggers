1e358347081f1a84ddb0743babd955a5d8026a7d
Carl Maxwell <carl.maxwell@gmail.com>
2015-08-27 11:31:21 -0700

    pulling in the new three js sourcecode

--- src/createcube.js
+++ src/createcube.js
@@ -3,84 +3,180 @@
 //
 
 function createCube() {
+	var light = new THREE.Color( 0xffffff );
+	var shadow = new THREE.Color( 0x505050 );
+
 	var matrix = new THREE.Matrix4();
 
 	var pxGeometry = new THREE.PlaneGeometry( 100, 100 );
+	pxGeometry.faces[ 0 ].vertexColors = [ light, shadow, light ];
+	pxGeometry.faces[ 1 ].vertexColors = [ shadow, shadow, light ];
 	pxGeometry.faceVertexUvs[ 0 ][ 0 ][ 0 ].y = 0.5;
-	pxGeometry.faceVertexUvs[ 0 ][ 0 ][ 3 ].y = 0.5;
-	pxGeometry.applyMatrix( matrix.makeRotationY( PI / 2 ) );
+	pxGeometry.faceVertexUvs[ 0 ][ 0 ][ 2 ].y = 0.5;
+	pxGeometry.faceVertexUvs[ 0 ][ 1 ][ 2 ].y = 0.5;
+	pxGeometry.applyMatrix( matrix.makeRotationY( Math.PI / 2 ) );
 	pxGeometry.applyMatrix( matrix.makeTranslation( 50, 0, 0 ) );
 
 	var nxGeometry = new THREE.PlaneGeometry( 100, 100 );
+	nxGeometry.faces[ 0 ].vertexColors = [ light, shadow, light ];
+	nxGeometry.faces[ 1 ].vertexColors = [ shadow, shadow, light ];
 	nxGeometry.faceVertexUvs[ 0 ][ 0 ][ 0 ].y = 0.5;
-	nxGeometry.faceVertexUvs[ 0 ][ 0 ][ 3 ].y = 0.5;
-	nxGeometry.applyMatrix( matrix.makeRotationY( - PI / 2 ) );
+	nxGeometry.faceVertexUvs[ 0 ][ 0 ][ 2 ].y = 0.5;
+	nxGeometry.faceVertexUvs[ 0 ][ 1 ][ 2 ].y = 0.5;
+	nxGeometry.applyMatrix( matrix.makeRotationY( - Math.PI / 2 ) );
 	nxGeometry.applyMatrix( matrix.makeTranslation( - 50, 0, 0 ) );
 
 	var pyGeometry = new THREE.PlaneGeometry( 100, 100 );
+	pyGeometry.faces[ 0 ].vertexColors = [ light, light, light ];
+	pyGeometry.faces[ 1 ].vertexColors = [ light, light, light ];
 	pyGeometry.faceVertexUvs[ 0 ][ 0 ][ 1 ].y = 0.5;
-	pyGeometry.faceVertexUvs[ 0 ][ 0 ][ 2 ].y = 0.5;
-	pyGeometry.applyMatrix( matrix.makeRotationX( - PI / 2 ) );
+	pyGeometry.faceVertexUvs[ 0 ][ 1 ][ 0 ].y = 0.5;
+	pyGeometry.faceVertexUvs[ 0 ][ 1 ][ 1 ].y = 0.5;
+	pyGeometry.applyMatrix( matrix.makeRotationX( - Math.PI / 2 ) );
 	pyGeometry.applyMatrix( matrix.makeTranslation( 0, 50, 0 ) );
 
-	var nyGeometry = new THREE.PlaneGeometry( 100, 100 );
-	nyGeometry.faceVertexUvs[ 0 ][ 0 ][ 1 ].y = 0.5;
-	nyGeometry.faceVertexUvs[ 0 ][ 0 ][ 2 ].y = 0.5;
-	nyGeometry.applyMatrix( matrix.makeRotationX( PI / 2 ) );
-	nyGeometry.applyMatrix( matrix.makeTranslation( 0, -50, 0 ) );
+	var py2Geometry = new THREE.PlaneGeometry( 100, 100 );
+	py2Geometry.faces[ 0 ].vertexColors = [ light, light, light ];
+	py2Geometry.faces[ 1 ].vertexColors = [ light, light, light ];
+	py2Geometry.faceVertexUvs[ 0 ][ 0 ][ 1 ].y = 0.5;
+	py2Geometry.faceVertexUvs[ 0 ][ 1 ][ 0 ].y = 0.5;
+	py2Geometry.faceVertexUvs[ 0 ][ 1 ][ 1 ].y = 0.5;
+	py2Geometry.applyMatrix( matrix.makeRotationX( - Math.PI / 2 ) );
+	py2Geometry.applyMatrix( matrix.makeRotationY( Math.PI / 2 ) );
+	py2Geometry.applyMatrix( matrix.makeTranslation( 0, 50, 0 ) );
 
 	var pzGeometry = new THREE.PlaneGeometry( 100, 100 );
+	pzGeometry.faces[ 0 ].vertexColors = [ light, shadow, light ];
+	pzGeometry.faces[ 1 ].vertexColors = [ shadow, shadow, light ];
 	pzGeometry.faceVertexUvs[ 0 ][ 0 ][ 0 ].y = 0.5;
-	pzGeometry.faceVertexUvs[ 0 ][ 0 ][ 3 ].y = 0.5;
+	pzGeometry.faceVertexUvs[ 0 ][ 0 ][ 2 ].y = 0.5;
+	pzGeometry.faceVertexUvs[ 0 ][ 1 ][ 2 ].y = 0.5;
 	pzGeometry.applyMatrix( matrix.makeTranslation( 0, 0, 50 ) );
 
 	var nzGeometry = new THREE.PlaneGeometry( 100, 100 );
+	nzGeometry.faces[ 0 ].vertexColors = [ light, shadow, light ];
+	nzGeometry.faces[ 1 ].vertexColors = [ shadow, shadow, light ];
 	nzGeometry.faceVertexUvs[ 0 ][ 0 ][ 0 ].y = 0.5;
-	nzGeometry.faceVertexUvs[ 0 ][ 0 ][ 3 ].y = 0.5;
-	nzGeometry.applyMatrix( matrix.makeRotationY( PI ) );
-	nzGeometry.applyMatrix( matrix.makeTranslation( 0, 0, -50 ) );
+	nzGeometry.faceVertexUvs[ 0 ][ 0 ][ 2 ].y = 0.5;
+	nzGeometry.faceVertexUvs[ 0 ][ 1 ][ 2 ].y = 0.5;
+	nzGeometry.applyMatrix( matrix.makeRotationY( Math.PI ) );
+	nzGeometry.applyMatrix( matrix.makeTranslation( 0, 0, - 50 ) );
 
-	var outputGeometry = new THREE.Geometry();
+	var geometry = new THREE.Geometry();
 	var dummy = new THREE.Mesh();
 
 	for ( var z = 0; z < worldDepth; z ++ ) {
 
 		for ( var x = 0; x < worldWidth; x ++ ) {
 
-			var tallness = map.getHeight_internal( x, z );
+			var h = getY( x, z );
 
-			dummy.position.x = x        * 100 - worldHalfWidth * 100;
-			dummy.position.y = tallness * 100;
-			dummy.position.z = z        * 100 - worldHalfDepth * 100;
+			matrix.makeTranslation(
+				x * 100 - worldHalfWidth * 100,
+				h * 100,
+				z * 100 - worldHalfDepth * 100
+			);
 
-			var px = map.getHeight_internal( x + 1, z );
-			var nx = map.getHeight_internal( x - 1, z );
-			var pz = map.getHeight_internal( x, z + 1 );
-			var nz = map.getHeight_internal( x, z - 1 );
+			var px = getY( x + 1, z );
+			var nx = getY( x - 1, z );
+			var pz = getY( x, z + 1 );
+			var nz = getY( x, z - 1 );
 
-			var mergeGeometries = function(pnGeometry) {
-				dummy.geometry = pnGeometry;
-					THREE.GeometryUtils.merge(outputGeometry, dummy);
-			};
+			var pxpz = getY( x + 1, z + 1 );
+			var nxpz = getY( x - 1, z + 1 );
+			var pxnz = getY( x + 1, z - 1 );
+			var nxnz = getY( x - 1, z - 1 );
 
-			mergeGeometries(pyGeometry);
-			mergeGeometries(nyGeometry);
+			var a = nx > h || nz > h || nxnz > h ? 0 : 1;
+			var b = nx > h || pz > h || nxpz > h ? 0 : 1;
+			var c = px > h || pz > h || pxpz > h ? 0 : 1;
+			var d = px > h || nz > h || pxnz > h ? 0 : 1;
 
-			//if ( ( px != tallness && px != tallness + 1 ) || x == 0 )
-				mergeGeometries(pxGeometry);
+			if ( a + c > b + d ) {
 
-			//if ( ( nx != tallness && nx != tallness + 1 ) || x == worldWidth - 1 )
-				mergeGeometries(nxGeometry);
+				var colors = py2Geometry.faces[ 0 ].vertexColors;
+				colors[ 0 ] = b === 0 ? shadow : light;
+				colors[ 1 ] = c === 0 ? shadow : light;
+				colors[ 2 ] = a === 0 ? shadow : light;
 
-			//if ( ( pz != talness && pz != talness + 1 ) || z == worldDepth - 1 )
-				mergeGeometries(pzGeometry);
+				var colors = py2Geometry.faces[ 1 ].vertexColors;
+				colors[ 0 ] = c === 0 ? shadow : light;
+				colors[ 1 ] = d === 0 ? shadow : light;
+				colors[ 2 ] = a === 0 ? shadow : light;
 
-			//if ( ( nz != tallness && nz != tallness + 1 ) || z == 0 )
-				mergeGeometries(nzGeometry);
+				geometry.merge( py2Geometry, matrix );
+
+			} else {
+
+				var colors = pyGeometry.faces[ 0 ].vertexColors;
+				colors[ 0 ] = a === 0 ? shadow : light;
+				colors[ 1 ] = b === 0 ? shadow : light;
+				colors[ 2 ] = d === 0 ? shadow : light;
+
+				var colors = pyGeometry.faces[ 1 ].vertexColors;
+				colors[ 0 ] = b === 0 ? shadow : light;
+				colors[ 1 ] = c === 0 ? shadow : light;
+				colors[ 2 ] = d === 0 ? shadow : light;
+
+				geometry.merge( pyGeometry, matrix );
+
+			}
+
+			if ( ( px != h && px != h + 1 ) || x == 0 ) {
+
+				var colors = pxGeometry.faces[ 0 ].vertexColors;
+				colors[ 0 ] = pxpz > px && x > 0 ? shadow : light;
+				colors[ 2 ] = pxnz > px && x > 0 ? shadow : light;
+
+				var colors = pxGeometry.faces[ 1 ].vertexColors;
+				colors[ 2 ] = pxnz > px && x > 0 ? shadow : light;
+
+				geometry.merge( pxGeometry, matrix );
+
+			}
+
+			if ( ( nx != h && nx != h + 1 ) || x == worldWidth - 1 ) {
+
+				var colors = nxGeometry.faces[ 0 ].vertexColors;
+				colors[ 0 ] = nxnz > nx && x < worldWidth - 1 ? shadow : light;
+				colors[ 2 ] = nxpz > nx && x < worldWidth - 1 ? shadow : light;
+
+				var colors = nxGeometry.faces[ 1 ].vertexColors;
+				colors[ 2 ] = nxpz > nx && x < worldWidth - 1 ? shadow : light;
+
+				geometry.merge( nxGeometry, matrix );
+
+			}
+
+			if ( ( pz != h && pz != h + 1 ) || z == worldDepth - 1 ) {
+
+				var colors = pzGeometry.faces[ 0 ].vertexColors;
+				colors[ 0 ] = nxpz > pz && z < worldDepth - 1 ? shadow : light;
+				colors[ 2 ] = pxpz > pz && z < worldDepth - 1 ? shadow : light;
+
+				var colors = pzGeometry.faces[ 1 ].vertexColors;
+				colors[ 2 ] = pxpz > pz && z < worldDepth - 1 ? shadow : light;
+
+				geometry.merge( pzGeometry, matrix );
+
+			}
+
+			if ( ( nz != h && nz != h + 1 ) || z == 0 ) {
+
+				var colors = nzGeometry.faces[ 0 ].vertexColors;
+				colors[ 0 ] = pxnz > nz && z > 0 ? shadow : light;
+				colors[ 2 ] = nxnz > nz && z > 0 ? shadow : light;
+
+				var colors = nzGeometry.faces[ 1 ].vertexColors;
+				colors[ 2 ] = nxnz > nz && z > 0 ? shadow : light;
+
+				geometry.merge( nzGeometry, matrix );
+
+			}
 
 		}
 
 	}
 
-	return outputGeometry;
+	return geometry;
 }
--- src/js/controls/FirstPersonControls.js
+++ src/js/controls/FirstPersonControls.js
@@ -2,59 +2,48 @@
  * @author mrdoob / http://mrdoob.com/
  * @author alteredq / http://alteredqualia.com/
  * @author paulirish / http://paulirish.com/
- * @author Carl Maxwell
-*/
+ */
 
 THREE.FirstPersonControls = function ( object, domElement ) {
-	this.camera = object;
-	this.lookTarget = new THREE.Vector3( 0, 0, 0 );
+
+	this.object = object;
+	this.target = new THREE.Vector3( 0, 0, 0 );
 
 	this.domElement = ( domElement !== undefined ) ? domElement : document;
 
-	this.movementSpeed = 0.001;
+	this.enabled = true;
+
+	this.movementSpeed = 1.0;
 	this.lookSpeed = 0.005;
 
 	this.lookVertical = true;
-	this.autoForward  = false;
-	// this.invertVertical = false;
+	this.autoForward = false;
 
 	this.activeLook = true;
 
 	this.heightSpeed = false;
-	this.heightCoef  = 1.0;
-	this.heightMin   = 0.0;
-	this.heightMax   = 1.0;
+	this.heightCoef = 1.0;
+	this.heightMin = 0.0;
+	this.heightMax = 1.0;
 
 	this.constrainVertical = false;
 	this.verticalMin = 0;
 	this.verticalMax = Math.PI;
 
+	this.autoSpeedFactor = 0.0;
+
 	this.mouseX = 0;
 	this.mouseY = 0;
-	this.mouseSensitivity = 0.5;
 
-	this.lat   = 0;
-	this.lon   = 0;
-	this.phi   = 0;
+	this.lat = 0;
+	this.lon = 0;
+	this.phi = 0;
 	this.theta = 0;
 
-  // keypress booleans:
-	this.moveForward  = false;
+	this.moveForward = false;
 	this.moveBackward = false;
-	this.moveLeft     = false;
-	this.moveRight    = false;
-	this.freeze       = false;
-  this.jump         = false;
-
-  // keypress-related player state
-  this.canJump      = true;
-	this.velocity = new Vector3;
-  this.move = {
-    magnitude   : new Timer(3),
-    accelerating: false       ,
-    direction   : new Vector3
-  };
-  this.move.magnitude.reverse();
+	this.moveLeft = false;
+	this.moveRight = false;
 
 	this.mouseDragOn = false;
 
@@ -62,7 +51,9 @@ THREE.FirstPersonControls = function ( object, domElement ) {
 	this.viewHalfY = 0;
 
 	if ( this.domElement !== document ) {
+
 		this.domElement.setAttribute( 'tabindex', -1 );
+
 	}
 
 	//
@@ -70,214 +61,218 @@ THREE.FirstPersonControls = function ( object, domElement ) {
 	this.handleResize = function () {
 
 		if ( this.domElement === document ) {
+
 			this.viewHalfX = window.innerWidth / 2;
 			this.viewHalfY = window.innerHeight / 2;
+
 		} else {
+
 			this.viewHalfX = this.domElement.offsetWidth / 2;
 			this.viewHalfY = this.domElement.offsetHeight / 2;
+
 		}
 
 	};
 
-	this.onMouseMove = function ( e ) {
-		var movementX = e.movementX       ||
-		                e.mozMovementX    ||
-		                e.webkitMovementX,
+	this.onMouseDown = function ( event ) {
 
-		movementY = e.movementY       ||
-		            e.mozMovementY    ||
-		            e.webkitMovementY;
+		if ( this.domElement !== document ) {
 
-		this.mouseX += movementX * this.mouseSensitivity;
-		this.mouseY += movementY * this.mouseSensitivity;
+			this.domElement.focus();
+
+		}
+
+		event.preventDefault();
+		event.stopPropagation();
+
+		if ( this.activeLook ) {
+
+			switch ( event.button ) {
+
+				case 0: this.moveForward = true; break;
+				case 2: this.moveBackward = true; break;
+
+			}
+
+		}
+
+		this.mouseDragOn = true;
+
+	};
+
+	this.onMouseUp = function ( event ) {
+
+		event.preventDefault();
+		event.stopPropagation();
+
+		if ( this.activeLook ) {
+
+			switch ( event.button ) {
+
+				case 0: this.moveForward = false; break;
+				case 2: this.moveBackward = false; break;
+
+			}
+
+		}
+
+		this.mouseDragOn = false;
+
+	};
+
+	this.onMouseMove = function ( event ) {
+
+		if ( this.domElement === document ) {
+
+			this.mouseX = event.pageX - this.viewHalfX;
+			this.mouseY = event.pageY - this.viewHalfY;
+
+		} else {
+
+			this.mouseX = event.pageX - this.domElement.offsetLeft - this.viewHalfX;
+			this.mouseY = event.pageY - this.domElement.offsetTop - this.viewHalfY;
+
+		}
+
 	};
 
 	this.onKeyDown = function ( event ) {
+
 		//event.preventDefault();
 
 		switch ( event.keyCode ) {
-			case 38: /*up*/    case 87: /*W*/ this.moveForward  = true; break;
 
-			case 37: /*left*/  case 65: /*A*/ this.moveLeft     = true; break;
+			case 38: /*up*/
+			case 87: /*W*/ this.moveForward = true; break;
 
-			case 40: /*down*/  case 83: /*S*/ this.moveBackward = true; break;
+			case 37: /*left*/
+			case 65: /*A*/ this.moveLeft = true; break;
 
-			case 39: /*right*/ case 68: /*D*/ this.moveRight    = true; break;
+			case 40: /*down*/
+			case 83: /*S*/ this.moveBackward = true; break;
 
-			case 81: /*Q*/ this.freeze = !this.freeze; break;
+			case 39: /*right*/
+			case 68: /*D*/ this.moveRight = true; break;
 
-      case 32: /*space*/ this.jump = this.canJump; break;
+			case 82: /*R*/ this.moveUp = true; break;
+			case 70: /*F*/ this.moveDown = true; break;
+
 		}
+
 	};
 
 	this.onKeyUp = function ( event ) {
-		switch( event.keyCode ) {
-			case 38: /*up*/    case 87: /*W*/ this.moveForward  = false; break;
 
-			case 37: /*left*/  case 65: /*A*/ this.moveLeft     = false; break;
+		switch ( event.keyCode ) {
 
-			case 40: /*down*/  case 83: /*S*/ this.moveBackward = false; break;
+			case 38: /*up*/
+			case 87: /*W*/ this.moveForward = false; break;
 
-			case 39: /*right*/ case 68: /*D*/ this.moveRight    = false; break;
+			case 37: /*left*/
+			case 65: /*A*/ this.moveLeft = false; break;
 
-      case 32: /*space*/ this.jump = false; break;
+			case 40: /*down*/
+			case 83: /*S*/ this.moveBackward = false; break;
+
+			case 39: /*right*/
+			case 68: /*D*/ this.moveRight = false; break;
+
+			case 82: /*R*/ this.moveUp = false; break;
+			case 70: /*F*/ this.moveDown = false; break;
+
 		}
+
 	};
 
 	this.update = function( delta ) {
 
-		if ( this.freeze ) return;
+		if ( this.enabled === false ) return;
 
-		var position = this.camera.position;
+		if ( this.heightSpeed ) {
 
-    this.move.direction
-      .set(0, 0, 0)
-      .add(this.moveForward  ? new Vector3(  0, 0, -1) : 0)
-      .add(this.moveBackward ? new Vector3(  0, 0,  1) : 0)
-      .add(this.moveRight    ? new Vector3(  1, 0,  0) : 0)
-      .add(this.moveLeft     ? new Vector3( -1, 0,  0) : 0);
+			var y = THREE.Math.clamp( this.object.position.y, this.heightMin, this.heightMax );
+			var heightDelta = y - this.heightMin;
 
-    if (this.move.direction.length() && !this.move.accelerating) {
-      this.move.accelerating = true;
-      this.move.magnitude.reverse();
-      this.move.magnitude.setDuration(3);
-    } else if (!this.move.direction.length() && this.move.accelerating) {
-      this.move.accelerating = false;
-      this.move.magnitude.reverse();
-      this.move.magnitude.setDuration(0.4);
-    }
+			this.autoSpeedFactor = delta * ( heightDelta * this.heightCoef );
 
-		var moveSpeed = function(timestamp) {
-			if (timestamp == false) return 0;
+		} else {
 
-      return Ease(timestamp.get(), "outSine") * h("1 seconds walk");
-		};
+			this.autoSpeedFactor = 0.0;
 
-		var decelerateSpeed = function(speed) {
-			//if (abs(speed) < 0.01) return 0;
+		}
 
-			return Ease(abs(speed)/h("1 seconds walk"), "inSine") * speed;
-		};
+		var actualMoveSpeed = delta * this.movementSpeed;
 
-		//this.velocity.x = decelerateSpeed(this.velocity.x);
-		//this.velocity.z = decelerateSpeed(this.velocity.z);
+		if ( this.moveForward || ( this.autoForward && !this.moveBackward ) ) this.object.translateZ( - ( actualMoveSpeed + this.autoSpeedFactor ) );
+		if ( this.moveBackward ) this.object.translateZ( actualMoveSpeed );
 
-		//if ( this.moveForward  && !this.moveBackward ) this.velocity.z  = -moveSpeed(this.moveForward );
-		//if ( this.moveBackward && !this.moveForward  ) this.velocity.z  =  moveSpeed(this.moveBackward);
+		if ( this.moveLeft ) this.object.translateX( - actualMoveSpeed );
+		if ( this.moveRight ) this.object.translateX( actualMoveSpeed );
 
-		//if ( this.moveLeft    && !this.moveRight     )  this.velocity.x = -moveSpeed(this.moveLeft    );
-		//if ( this.moveRight   && !this.moveLeft      )  this.velocity.x =  moveSpeed(this.moveRight   );
-
-    //this.velocity.x = Ease(this.move.magnitude.get(), "outSine") * h("1 seconds walk") * this.move.direction.x;
-    this.velocity.z = Ease(this.move.magnitude.get(), "outSine") * h("1 seconds walk") * (this.move.direction.z || sign(this.velocity.z));
-
-    //
-    // check for collisions
-    //
-
-    // TODO I'd feel better if this used this.camera.rotation.x or the like instead of mouse position
-    var theta = THREE.Math.degToRad(this.mouseX);
-    var radius = sqrt( pow(2, this.velocity.x) + pow(2, this.velocity.z) );
-    var right_angle = THREE.Math.degToRad(90);
-
-    var impulse = new Vector3(
-      (cos(theta)*-this.velocity.z + cos(theta+right_angle)*this.velocity.x)*delta,
-      0,
-      (sin(theta)*-this.velocity.z + sin(theta+right_angle)*this.velocity.x)*delta
-    );
-
-    var forward = impulse.clone().normalize().multiply(0.07*h("1 block"));
-
-    var check = function(dir) {
-      return lineTrace(
-        position.clone().sub(new Vector3(0, character_height - 0.5*h("1 block"), 0)),
-        dir
-      );
-    };
-
-    // TODO allow sliding collisions
-
-    if (check(forward)) {
-      impulse = new Vector3(0, 0, 0);
-    }
-
-    position.add(impulse);
-
-    //
-    // orient the camera
-    //
+		if ( this.moveUp ) this.object.translateY( actualMoveSpeed );
+		if ( this.moveDown ) this.object.translateY( - actualMoveSpeed );
 
 		var actualLookSpeed = delta * this.lookSpeed;
 
-		if ( !this.activeLook ) actualLookSpeed = 0;
+		if ( !this.activeLook ) {
+
+			actualLookSpeed = 0;
+
+		}
 
 		var verticalLookRatio = 1;
 
-		if ( this.constrainVertical )
-			verticalLookRatio = PI / ( this.verticalMax - this.verticalMin );
+		if ( this.constrainVertical ) {
 
-		this.lon = this.mouseX;
-		if( this.lookVertical ) this.lat = -this.mouseY;
+			verticalLookRatio = Math.PI / ( this.verticalMax - this.verticalMin );
 
-		this.lat = max( - 85, min( 85, this.lat ) );
+		}
+
+		this.lon += this.mouseX * actualLookSpeed;
+		if ( this.lookVertical ) this.lat -= this.mouseY * actualLookSpeed * verticalLookRatio;
+
+		this.lat = Math.max( - 85, Math.min( 85, this.lat ) );
 		this.phi = THREE.Math.degToRad( 90 - this.lat );
 
 		this.theta = THREE.Math.degToRad( this.lon );
 
-		if ( this.constrainVertical )
+		if ( this.constrainVertical ) {
+
 			this.phi = THREE.Math.mapLinear( this.phi, 0, Math.PI, this.verticalMin, this.verticalMax );
 
-		var targetPosition = this.lookTarget;
+		}
 
-		this.lookTarget.x = position.x + h("1 block") * sin( this.phi ) * cos( this.theta );
-		this.lookTarget.y = position.y + h("1 block") * cos( this.phi );
-		this.lookTarget.z = position.z + h("1 block") * sin( this.phi ) * sin( this.theta );
+		var targetPosition = this.target,
+			position = this.object.position;
 
-		this.camera.lookAt( this.lookTarget );
+		targetPosition.x = position.x + 100 * Math.sin( this.phi ) * Math.cos( this.theta );
+		targetPosition.y = position.y + 100 * Math.cos( this.phi );
+		targetPosition.z = position.z + 100 * Math.sin( this.phi ) * Math.sin( this.theta );
 
-    //
-    // deal with vertical motion
-    //
+		this.object.lookAt( targetPosition );
 
-    var breath = (0.007 * sin((new Date).getTime() * PI / 1000 / 4));
-
-		var verticalPosition = (map.getHeight(position.x, position.z) + character_height + breath);
-
-    if (this.velocity.y > 0) {
-      this.camera.position.y += this.velocity.y * delta;
-
-      this.velocity.y -= gravity * delta;
-    } else if (!lineTrace(camera.position, new Vector3(0, -(character_height + 0.01), 0))) {
-      this.velocity.y -= gravity * delta;
-
-      this.camera.position.y += this.velocity.y  * delta;
-    } else {
-      this.camera.position.y = verticalPosition;
-
-      if (this.jump) {
-        this.velocity.y += h("3 blocks") + gravity * 0.2;
-        this.jump = false;
-        this.canJump = false;
-      } else {
-        this.velocity.y = 0;
-        this.canJump = true;
-      }
-    }
 	};
+
 
 	this.domElement.addEventListener( 'contextmenu', function ( event ) { event.preventDefault(); }, false );
 
 	this.domElement.addEventListener( 'mousemove', bind( this, this.onMouseMove ), false );
-	//this.domElement.addEventListener( 'mousedown', bind( this, this.onMouseDown ), false );
-	//this.domElement.addEventListener( 'mouseup'  , bind( this, this.onMouseUp   ), false );
-	this.domElement.addEventListener( 'keydown'  , bind( this, this.onKeyDown   ), false );
-	this.domElement.addEventListener( 'keyup'    , bind( this, this.onKeyUp     ), false );
+	this.domElement.addEventListener( 'mousedown', bind( this, this.onMouseDown ), false );
+	this.domElement.addEventListener( 'mouseup', bind( this, this.onMouseUp ), false );
+
+	window.addEventListener( 'keydown', bind( this, this.onKeyDown ), false );
+	window.addEventListener( 'keyup', bind( this, this.onKeyUp ), false );
 
 	function bind( scope, fn ) {
+
 		return function () {
+
 			fn.apply( scope, arguments );
+
 		};
+
 	};
 
 	this.handleResize();
+
 };
