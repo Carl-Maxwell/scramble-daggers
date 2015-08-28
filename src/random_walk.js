36089cf77d821181df3f8b777e2ddb6baac4c064
Carl Maxwell <carl.maxwell@gmail.com>
2015-08-27 22:41:49 -0700

    code style

--- src/random_walk.js
+++ src/random_walk.js
@@ -1,9 +1,7 @@
 
 
 function generateHeight( width, height ) {
-
 	/*
-	
 	var data = [], perlin = new ImprovedNoise(),
 	size = width * height, quality = 2, z = Rng() * 100;
 
@@ -20,15 +18,14 @@ function generateHeight( width, height ) {
 
 	}
 	*/
-	
-	var Agent = function() {
 
+	var Agent = function() {
 		var BLOCK_SIZE = 1,
 		    WIDTH      = worldWidth,
 		    HEIGHT     = worldDepth;
-		
+
 		this.map = [];
-		
+
 		for (var i = 0; i < WIDTH * HEIGHT; i++) this.map[i] = 0;
 
 		this.chances = [500, 500, 500, 500];
@@ -64,7 +61,7 @@ function generateHeight( width, height ) {
 						case 1: x += BLOCK_SIZE; break;
 						case 2: y += BLOCK_SIZE; break;
 						case 3: x -= BLOCK_SIZE; break;
-					} 
+					}
 				}
 				valueSum += value;
 			}, this );
@@ -80,20 +77,19 @@ function generateHeight( width, height ) {
 
 			this.map[ entity_key ] = 60; //|| round( (248 - this.map[ entity_key ]) * 0.01 );
 
-//						entities[ entity_key ].sprite.color[0] += round( (248 - rgb[0]) * 0.2 );
-//						entities[ entity_key ].sprite.color[1] += round( (187 - rgb[1]) * 0.2 );
-//						entities[ entity_key ].sprite.color[2] += round( (198 - rgb[2]) * 0.2 );
+//		entities[ entity_key ].sprite.color[0] += round( (248 - rgb[0]) * 0.2 );
+//		entities[ entity_key ].sprite.color[1] += round( (187 - rgb[1]) * 0.2 );
+//		entities[ entity_key ].sprite.color[2] += round( (198 - rgb[2]) * 0.2 );
 
-		}
-		
+		};
+
 		this.destroy = function() {
 			for (var i = 0; i < WIDTH * HEIGHT; i++) {
-			
 				var x = i % WIDTH,
 				    y = (i-x)/WIDTH;
-				
+
 				// var dist = pow(2, x - WIDTH / 2) + pow(2, y - HEIGHT/2);
-				
+
 				if (this.map[i] == 0) {
 					var any = false;
 					for (var q=-1; q<2; q++)
@@ -101,15 +97,13 @@ function generateHeight( width, height ) {
 							any = any || this.map[i + (q + w*WIDTH)] == 60;
 					this.map[i] = any ? 65 : 70;
 				}
-				
+
 				// this.map[i] = this.map[i] || max(min(random() * 255 + random() * 255, random() * 255 + random() * 255) / 2, 100);
 				//round( sin( abs((x - WIDTH/2) / (WIDTH/2) ) * abs((y - HEIGHT/2) / (HEIGHT/2)) * (PI/2) ) * 60 );
-				
 			}
 		};
-		
 	};
-	
+
 	var agent = new Agent();
 
 	var iterations = 6000 + (Rng() * 2000);
@@ -120,7 +114,4 @@ function generateHeight( width, height ) {
 	agent.destroy();
 
 	return agent.map;
-
 }
-
-
