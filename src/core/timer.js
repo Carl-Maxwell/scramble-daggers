2df552f566be0f918b58d59564bf6d6d096b7b77
Carl Maxwell <carl.maxwell@gmail.com>
2015-08-29 22:10:44 -0700

    coding style
    * fix indentation in rng.js
    * pull Timer member functions into prototypes

--- src/core/timer.js
+++ src/core/timer.js
@@ -2,52 +2,56 @@
 window.gameStart = ((new Date()).getTime())/1000;
 
 window.Timer = function(duration) {
-	this.start     = new timestamp();
+	this.start     = new Timestamp();
 	this.duration  = duration || 1;
 	this.direction = -1;
-
-	this.get = function() {
-		return abs(clamp(this.start.distance() / this.duration, this.direction == -1 ? 0 : -1, this.direction == -1 ? 1 : 0));
-	};
-
-	this.reverse = function() {
-		var progress = this.get();
-
-		this.direction *= -1;
-
-		this.start = new timestamp();
-
-		this.start.value += this.direction * (this.duration * progress);
-
-		return this;
-	};
-
-	this.setDuration = function(newDuration) {
-		var oldDuration = this.duration;
-		var progress    = this.get();
-		var oldDistance = this.start.distance();
-
-	this.start = new timestamp();
-
-		this.start.value -= oldDistance/(oldDuration/newDuration);
-		this.duration = newDuration;
-
-		return this;
-	};
 
 	return this;
 };
 
-function timestamp() {
-	this.value = (new Date()).getTime()/1000 - window.gameStart;
+window.Timer.prototype.get = function() {
+	return abs(clamp(
+		this.start.distance() / this.duration,
+		this.direction == -1 ? 0 : -1,
+		this.direction == -1 ? 1 : 0
+	));
+};
 
-	this.distance = function(then) {
-		if (typeof then == "undefined") then = new timestamp;
+window.Timer.prototype.reverse = function() {
+	var progress = this.get();
 
-		return then.value - this.value;
-	};
+	this.direction *= -1;
+
+	this.start = new Timestamp();
+
+	this.start.value += this.direction * (this.duration * progress);
 
 	return this;
-}
+};
+
+window.Timer.prototype.setDuration = function(newDuration) {
+	var oldDuration = this.duration;
+	var progress    = this.get();
+	var oldDistance = this.start.distance();
+
+	this.start = new Timestamp();
+
+	this.start.value -= oldDistance/(oldDuration/newDuration);
+	this.duration = newDuration;
+
+	return this;
+};
+
+window.Timestamp = function() {
+	this.value = (new Date()).getTime()/1000 - window.gameStart;
+
+	return this;
+};
+
+window.Timestamp.prototype.distance = function(then) {
+	if (typeof then == "undefined") then = new Timestamp();
+
+	return then.value - this.value;
+};
 
 // TODO need some unit tests to make sure this is actually working right
